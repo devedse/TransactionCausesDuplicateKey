f258e0a19a8c67846425dac3827df8ad14baf599
Devedse <davydavidse@gmail.com>
2020-03-06 13:33:47 +0100

    Method order

--- TransactionCausesDuplicateKey/Services/PutThingsInDbService.cs
+++ TransactionCausesDuplicateKey/Services/PutThingsInDbService.cs
@@ -2,8 +2,6 @@
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using TransactionCausesDuplicateKey.Db;
@@ -21,6 +19,19 @@ namespace TransactionCausesDuplicateKey.Services
             _configuration = configuration;
             _logger = logger;
             _dbContext = dbContext;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            //This is the first event and should complete successfully
+            await ProcessNewhireEvent("TEST1", "John", "0001", "0001");
+
+            //This is an event with a unique event id but the same person details. It should fail on a duplicate key for the Employee Table
+            await ProcessNewhireEvent("TEST2", "John", "0001", "0001");
+            //This is as expected
+
+            //When we now submit a new completely unrelated event, it still fails on trying to add the previous event
+            await ProcessNewhireEvent("TEST3", "Henk", "0002", "0002");
         }
 
         public async Task ProcessNewhireEvent(string eventId, string personName, string personId, string personNumber)
@@ -60,20 +71,6 @@ namespace TransactionCausesDuplicateKey.Services
             {
                 _logger.LogError(ex.InnerException.Message);
             }
-        }
-
-
-        public async Task StartAsync(CancellationToken cancellationToken)
-        {
-            //This is the first event and should complete successfully
-            await ProcessNewhireEvent("TEST1", "John", "0001", "0001");
-
-            //This is an event with a unique event id but the same person details. It should fail on a duplicate key for the Employee Table
-            await ProcessNewhireEvent("TEST2", "John", "0001", "0001");
-            //This is as expected
-
-            //When we now submit a new completely unrelated event, it still fails on trying to add the previous event
-            await ProcessNewhireEvent("TEST3", "Henk", "0002", "0002");
         }
 
         public async Task StopAsync(CancellationToken cancellationToken)
